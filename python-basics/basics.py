e94a8aa3ba6d26671076bda027a4b6cf7e0848ae
arnobdas-dev <arnobdas.dev@gmail.com>
2026-01-07 05:30:19 +0600

    Merge remote changes with local updates

--- python-basics/basics.py
+++ python-basics/basics.py
@@ -1,2 +1,40 @@
 print("Let's start our jurny")
 print("---------------------")
+
+x = 4
+y = 3
+# a= int(23)
+# a= float(23)
+# a= complex(23)
+# a = None
+# a = True
+# a = 10
+# print(isinstance(a,int))
+# x=[1,2,3]
+# print(dir(x))
+
+# print(id(x))
+# print(id(y))
+# help(object)
+# code = """
+# for i in range(3):
+#     print("Hello", i)
+# """
+# exec(code)
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# p = Person("Alice", 25)
+# print(vars(p))
+def test():
+    a = 1
+    b = 2
+    print(locals())
+
+test()
+
+print(globals())
+
+# print(a)
