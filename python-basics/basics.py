9629277190b78d8136088de15e3d26199803911b
arnobdas-dev <arnobdas.dev@gmail.com>
2026-01-08 00:43:10 +0600

    python basics

--- python-basics/basics.py
+++ python-basics/basics.py
@@ -1,8 +1,16 @@
 print("Let's start our jurny")
 print("---------------------")
 
+# Variable, assignment Comments:
+# ------------------------------
+
 x = 4
 y = 3
+z=[1,2,3]
+i=0
+# a = "Look"
+# a = str(x)
+# print(type(a))
 # a= int(23)
 # a= float(23)
 # a= complex(23)
@@ -10,8 +18,15 @@ y = 3
 # a = True
 # a = 10
 # print(isinstance(a,int))
-# x=[1,2,3]
+
+
+
+# Global function:
+# -----------------
+# a = input("What do you want: ")
 # print(dir(x))
+# help(str)
+# print(len(x))
 
 # print(id(x))
 # print(id(y))
@@ -28,13 +43,42 @@ y = 3
 
 # p = Person("Alice", 25)
 # print(vars(p))
-def test():
-    a = 1
-    b = 2
-    print(locals())
+# def test():
+#     a = 1
+#     b = 2
+#     print(locals())
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-test()
-
-print(globals())
+# p = Person("Alice", 25)
+# print(vars(p))
+# test()
+# print(globals())
 
 # print(a)
+
+
+"""
+Operators
+---------
+"""
+# print(x+y)
+# x*=x
+# if (x != i):
+#     print("True")
+# text = "machine learning"
+# print("machine" in text)
+# print(2 in z)
+# print(12 not in z)
+# print(x is  y)
+# print(x is not y)
+# print(x<<3)
+# print(x<<2)
+# print(5 ^ 3)
+# nums = [1, 2, 3, 2, 1]
+# unique = 0
+# for n in nums:
+#     unique ^= n
+# print(unique)
